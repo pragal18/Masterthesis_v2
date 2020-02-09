b8db581a441aab7f61e4258f5f6493a81fad7289
pragal18 <36824197+pragal18@users.noreply.github.com>
2020-02-09 14:52:14 +0100

    Add files via upload

--- final_scripts/separation_context.py
+++ final_scripts/separation_context.py
@@ -4,16 +4,19 @@ import sys
 previousSubject=None
 path=sys.argv[1]
 check=path[len(path)-6:len(path)-4]
-#print(check)
+if check=="en":
+	i=29
+else:
+	i=32
 with open(path,encoding="utf-8") as openfileobject:
     for line in openfileobject:
         s=line.find('?')
-        thisSubject=line[32:s]
+        thisSubject=line[i:s]
         if previousSubject==thisSubject:
-            f.write(line);
+            f.write(line)
         else:
             try:  
                 f=open("Files/Input"+check+"/"+thisSubject+".ttl",'a',encoding="utf-8")
-                f.write(line);
+                f.write(line)
             except:
                 pass  
