1a5b6c735237bda8c2a328ebc52ea9ff75c6d559
pragal18 <36824197+pragal18@users.noreply.github.com>
2020-03-01 15:58:25 +0100

    Add files via upload

--- final_scripts/main.py
+++ final_scripts/main.py
@@ -1,45 +1,61 @@
 #!/usr/bin/python 
 import sentence_nltk, token_nltk, pos_nltk, sentence_sio, token_sio, pos_sio ,sentence_ttb , token_ttb, pos_ttb, sentence_pat
-import search_sentence, search_token, search_pos
+import search_sentence, search_token, search_pos , pos_nltk_lang , search_link
 import sys
 
+def getassign():
+	if lang=="en":
+		return "english"
+	elif lang=="fr":
+		return "french"
+	elif lang=="es":
+		return "spanish"
+	elif lang=="de":
+		return "german"
+	else:
+		return "english"
+	
 def exec():
-	if tool=="NLTK":
-		assign="english"
-		if task=="SEN":
+	if tool=="NLTK" or tool=="nltk":
+		assign=getassign()
+		#print(assign)
+		if task=="SEN" or task=="sen":
 			sentence_nltk.main(n,lang,assign)
-		elif task=="TOK":
+		elif task=="TOK" or task=="tok":
 			token_nltk.main(n,lang,assign)
-		elif task=="POS":
-			pos_nltk.main(n,lang,assign)
-		# elif task=="LINK":
-			# link_nltk.main(n,lang,assign)
+		elif task=="POS" or task=="pos":
+			if lang=="en" or lang=="ja" :
+				pos_nltk.main(n,lang,assign)
+			else:
+				pos_nltk_lang.main(n,lang,assign)
+		#elif task=="LINK":
+			#link_nltk.main(n,lang,assign)
 		else:
 			print("Invalid task")
-	elif tool=="SIO":
-		if task=="SEN":
+	elif tool=="SIO" or tool=="sio":
+		if task=="SEN" or task=="sen":
 			sentence_sio.main(n,lang)
-		elif task=="TOK":
+		elif task=="TOK" or task=="tok":
 			token_sio.main(n,lang)
-		elif task=="POS":
+		elif task=="POS" or task=="pos":
 			pos_sio.main(n,lang)
 		# elif task=="LINK":
 			# link_sio.main(n,task,search,tool,lang)
 		else:
 			print("Invalid task")	
-	elif tool=="TTB":
-		if task=="SEN":
+	elif tool=="TTB" or tool=="ttb":
+		if task=="SEN" or task=="sen":
 			sentence_ttb.main(n,lang)
-		elif task=="TOK":
+		elif task=="TOK" or task=="tok":
 			token_ttb.main(n,lang)
-		elif task=="POS":
+		elif task=="POS" or task=="pos":
 			pos_ttb.main(n,lang)
 		# elif task=="LINK":
 			# link_ttb.main(n,task,search,tool,lang)
 		else:
 			print("Invalid task")
-	elif tool=="PAT":
-		if task=="SEN":
+	elif tool=="PAT" or tool=="pat":
+		if task=="SEN" or task=="sen":
 			sentence_pat.main(n,lang)
 		# elif task=="TOK":
 			# token_ttb.main(n,lang)
@@ -51,14 +67,19 @@ def exec():
 		print("Invalid tool chosen")
 
 def exec_search():
-	if task=="SEN":
+	if task=="ALL" or task=="all":
 		search_sentence.main(lang,search)
-	elif task=="TOK":
 		search_token.main(lang,search)
-	elif task=="POS":
 		search_pos.main(lang,search)
-	# elif task=="LINK":
-		# link_ttb.main(n,task,search,tool,lang)
+		search_link.main(lang,search)
+	elif task=="SEN" or task=="sen":
+		search_sentence.main(lang,search)
+	elif task=="TOK" or task=="tok":
+		search_token.main(lang,search)
+	elif task=="POS" or task=="pos":
+		search_pos.main(lang,search)
+	elif task=="LINK":
+		search_link.main(lang,search)
 	else:
 		print("Invalid task")		
 
@@ -67,8 +88,8 @@ task=sys.argv[2]
 search=sys.argv[3]
 tool=sys.argv[4]
 lang=sys.argv[5]
+
 if search == "NA" :
 	exec()
 else:
 	exec_search()
-#submain2.main(n,task,search,tool,lang)
